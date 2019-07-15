e6e0e3f33a09dfccca2da5361ddd532d30330848
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-07-15 15:11:56 +0300

    added not null feature on log table fields and comment fields

--- db/migrate/20190715120648_create_logs.rb
+++ db/migrate/20190715120648_create_logs.rb
@@ -1,9 +1,9 @@
 class CreateLogs < ActiveRecord::Migration[5.2]
   def change
     create_table :logs do |t|
-      t.string :ulr
+      t.string :ulr, null: false
 
-      t.timestamps
+      t.timestamps :created_at, null: false
     end
   end
 end
