74991d884cff90a1fbbfb2837d4be9d7da6ad409
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-07-17 15:48:30 +0300

    fixed all trouble with rails db:migrate:status and rails s commands. added root welcome#index to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'welcome/index'
+
   get 'welcome/index'
   get 'welcome/signIn'
   get 'welcome/authentificate'
   post 'welcome/authentificate'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_07_12_075408) do
-=======
-ActiveRecord::Schema.define(version: 2019_07_16_071251) do
->>>>>>> auth
+ActiveRecord::Schema.define(version: 2019_07_17_093157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "last_name"
-    t.date "birthdate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.integer "integ"
-    t.bigint "biginteg"
-    t.decimal "numer"
-    t.decimal "decim"
-  end
-
-=======
   create_table "actions", force: :cascade do |t|
     t.string "type", null: false
   end
@@ -93,12 +74,17 @@ ActiveRecord::Schema.define(version: 2019_07_16_071251) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.string "password", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "categories", "users", column: "owner_id"
@@ -110,5 +96,4 @@ ActiveRecord::Schema.define(version: 2019_07_16_071251) do
   add_foreign_key "likes", "users"
   add_foreign_key "logs", "actions", column: "actions_id"
   add_foreign_key "logs", "users"
->>>>>>> auth
 end
