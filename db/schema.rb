f27015e2f810f50e9d8178193d6fbf2c8f4fa9ce
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-07-18 10:30:14 +0300

    after generate confirmable but before generating mailer (or berore install another)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_093157) do
+ActiveRecord::Schema.define(version: 2019_07_18_070102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,9 +73,9 @@ ActiveRecord::Schema.define(version: 2019_07_17_093157) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -83,6 +83,10 @@ ActiveRecord::Schema.define(version: 2019_07_17_093157) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
