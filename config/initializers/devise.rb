d0d9c2c31775078294124f118158337d270294e5
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-07-19 11:59:21 +0300

    Works facebook ouath
    But need to add sign in after omniauth sign up

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,5 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   # config.omniauth :facebook, "APP_ID", "APP_SECRET"
-  config.omniauth :facebook, "APP_ID", "APP_SECRET", token_params: { parse: :json }
+  config.omniauth :facebook, ENV['FACEBOOK_APPID'], ENV['FACEBOOK_SECRET'],
+                  callback_url: 'http://localhost:3000/users/auth/facebook/callback'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   get 'welcome/index'
   get 'welcome/signIn'
   get 'welcome/authentificate'
