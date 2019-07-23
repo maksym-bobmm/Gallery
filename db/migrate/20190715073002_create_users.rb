a53ebf84e7040da9e83e786ae63f968c6098240b
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-07-23 10:21:17 +0300

    fixed migrations: removes not null option from users table because it is not inputed from devise registration

--- db/migrate/20190715073002_create_users.rb
+++ db/migrate/20190715073002_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username, null: false
+      t.string :username, null: true
       # t.string :password, null: false
-      t.string :first_name, null: false
-      t.string :last_name, null: false
+      t.string :first_name, null: true
+      t.string :last_name, null: true
       # t.string :email, null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -75,9 +75,9 @@ ActiveRecord::Schema.define(version: 2019_07_22_123638) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
