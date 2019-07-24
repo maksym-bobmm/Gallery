04bd4ae9be70fb3c8b903cc49b2c1cf5e4b999b3
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-07-24 13:18:30 +0300

    added categories creation(need redo) and deletes ones

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,24 +1,48 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
-  # def show
-  # end
+  def show
+  end
 
   def new
-    render 'new'
-    cat = Category.new()
-    cat.name = params[:name]
-    cat.owner_id = current_user.id
-    if cat.save!
-      raise qwe
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new
+    @category.name = category_params[:name]
+    @category.owner_id = current_user.id
+    if @category.save!
+      redirect_to categories_path
+    else
+      redirect_to new_category_path
     end
+  end
+
+  # def create
+  #   @category = Category.new
+  #   @category.name = category_params[:name]
+  #   @category.owner_id = current_user.id
+  #   if @category.save!
+  #     redirect_to categories_path
+  #   else
+  #     redirect_to new_category_path
+  #   end
+  # end
+
+  def destroy
+    # raise qwe
+    @category = Category.find(params[:id])
+    @category.destroy
     redirect_to categories_path
   end
 
   private
 
   def category_params
-    params.require(:name).permit(:name)
+    params.require(:category).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   # devise_for :users
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   resources :categories
-  # get 'welcome/index'
-  # get 'welcome/signIn'
-  # get 'welcome/authentificate'
-  # post 'welcome/authentificate'
-  # get 'categories/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
