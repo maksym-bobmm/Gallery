f565dc5d1d60ee7e08605ff304dff86252790ad4
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-07-25 11:02:29 +0300

    changes migration add_reference_frop_images_to_categories.rb -- renamed reference_key from categories to category

--- db/migrate/20190722123638_add_reference_from_images_to_categories.rb
+++ db/migrate/20190722123638_add_reference_from_images_to_categories.rb
@@ -1,7 +1,7 @@
 class AddReferenceFromImagesToCategories < ActiveRecord::Migration[5.2]
   def change
     change_table :images do |t|
-      t.references :categories, null: true, foreign_key: true
+      t.references :category, null: true, foreign_key: true
     end
   end
 end
