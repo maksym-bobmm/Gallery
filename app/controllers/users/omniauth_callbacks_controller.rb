03e27ee2fd7ed9c2dc7f8771e9cb4f8f83b999df
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-08-01 18:03:30 +0300

    Moved session and omniauth controllers into user folder

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,8 @@
-class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
@@ -16,4 +20,27 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def failure
     redirect_to root_path
   end
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks', sessions: "sessions",
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: "users/sessions",
                                               confirmations: 'users/confirmations' }
   resources :categories
   resources :images
