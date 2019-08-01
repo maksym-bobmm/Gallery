5b2d682db7548bd9a6f5e8c9eb1e290895e0c075
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-08-01 10:36:09 +0300

    drop unused admins table

---
+++ db/migrate/20190801064929_delete_admins_table.rb
@@ -0,0 +1,5 @@
+class DeleteAdminsTable < ActiveRecord::Migration[5.2]
+  def change
+    drop_table(:admins, {})
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_122223) do
+ActiveRecord::Schema.define(version: 2019_08_01_064929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,16 +43,6 @@ ActiveRecord::Schema.define(version: 2019_07_31_122223) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "password", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "email", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
