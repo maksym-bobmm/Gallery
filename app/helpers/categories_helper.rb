bf3f6a3f1e19f62d3788d51d27df4b7b562e2e82
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-08-05 13:21:47 +0300

    Calc categories priority Display categories desc

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -15,4 +15,37 @@ module CategoriesHelper
     end
     cats.sort_by{|_,v| v}.reverse
   end
+
+  def fill_categories
+    category_with_rating = []
+    Category.all.each do |category|
+      # todo implement with active record
+      query = "select sum(total) from(
+                   select sum(images.rating) as total from categories
+                          left join images
+                          on categories.id = images.category_id
+                   where categories.id = #{category.id}
+
+                   union all
+
+                   select count(images)  from categories
+                           left join images
+                           on categories.id = images.category_id
+                   where categories.id = #{category.id}
+
+                   union all
+
+                   select count(comments) from comments
+                           left join images
+                               on comments.image_id = images.id
+                           left join categories
+                               on images.category_id = categories.id
+                   where categories.id = #{category.id}
+                ) as query"
+      rating = ActiveRecord::Base.connection.exec_query(query)
+      category_with_rating << {category: category, rating: rating.first['sum'].to_i}
+    end
+    # raise QWE
+    category_with_rating.sort_by { |hash| hash[:rating] }.reverse
+  end
 end
