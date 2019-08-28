de2cb0383b6163170aec8649fc3ca3c3da8fba0d
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-08-28 17:08:36 +0300

    Add slider

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@
 
 # application helper
 module ApplicationHelper
+  def find_categories_rating
+    category_with_rating = []
+    Category.find_each do |category|
+      sum = category.images.length
+      category.images.each do |image|
+        sum += image.likes_count
+        sum += image.comments_count
+      end
+      category_with_rating << { category: category, rating: sum }
+    end
+    category_with_rating.sort_by { |hash| hash[:rating] }.reverse
+  end
 end
