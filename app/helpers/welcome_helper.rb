9f6d115d6687ab67f626a3ad596a025337a53257
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-09-11 12:25:33 +0300

    Add footer

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,4 +2,16 @@
 
 # helper need to delete
 module WelcomeHelper
+  def find_popular_images
+    images = Image.order(:likes_count).reverse
+    index = 0
+    result_arr = []
+    while result_arr.size < 5 do
+      if images[index].path.width > images[index].path.height
+        result_arr << images[index]
+      end
+      index += 1
+    end
+    result_arr
+  end
 end
