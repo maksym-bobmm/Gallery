78a173b7d2c468bbc2075729260145848a5d6083
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-09-16 10:21:17 +0300

    Little adding

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -20,3 +20,42 @@
 //
 //         });
 // }));
+// #counter.counter
+//     .bl
+//     .number= "#{User.size}"
+//     .text= "#{t(:users_count)}"
+//     .bl
+//     .number= "#{Category.size}"
+//     .text= "#{t(:'site.stat.categories'}"
+//     .bl
+//     .number= "#{Image.size}"
+//     .text= "#{t(:'site.stat.likes'}"
+//     .bl
+//     .number= "#{Comment.size}"
+//     .text= "#{t(:'site.stat.comments')}"
+// $(document).on('turbolinks:load', function() {
+//     var cc = 1;
+//     $(window).scroll(function () {
+//         $('#counter').each(function(){
+//             var cPos = $(this).offset().top,
+//                 topWindow = $(window).scrollTop();
+//             if (cPos < topWindow + 150) {
+//                 if (cc < 2) {
+//                     $('.number').addClass('viz');
+//                     $('.number').each(function() {
+//                         $(this).prop('Counter',0).animate({
+//                             Counter:$(this).text()
+//                         },{
+//                             duration: 4000,
+//                             easing: 'swing',
+//                             step:function(now) {
+//                                 $(this).text(Math.ceil(now));
+//                             }
+//                         });
+//                     });
+//                     cc = cc + 1;
+//                 };
+//             };
+//         });
+//     });
+// });
