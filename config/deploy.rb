d86de5a50dadf734f2e2726e251f2eec1c5a7e18
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-09-17 11:47:30 +0300

    Add empty line(for check publick key works)

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :ssh_options, { :verbose => :debug }
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# 
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
