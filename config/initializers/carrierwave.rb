1b39e4e7310a3cdb763fa4ed3001733aca82072e
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-09-20 13:19:57 +0300

    Change some variables to Figaro.env 3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider: 'AWS',                             # required
-      aws_access_key_id: ENV.fetch('S3_KEY') ,           # required
-      aws_secret_access_key: ENV.fetch('S3_SECRET'),     # required
-      region: ENV.fetch('S3_REGION')                        # optional, defaults to 'us-east-1'
+      aws_access_key_id: Figaro.env.S3_KEY ,           # required
+      aws_secret_access_key: Figaro.env.S3_SECRET,     # required
+      region: Figaro.env.S3_REGION                        # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV.fetch('S3_BUCKET_NAME')               # required
+  config.fog_directory  = Figaro.env.S3_BUCKET_NAME               # required
   # config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   # config.fog_public     = false                                  # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
