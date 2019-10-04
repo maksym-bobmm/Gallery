00ba16801528462403e859573d0f340137c1aa18
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-10-04 17:43:27 +0300

    Added 2 tests, bun arent work

--- spec/features/subscription_spec.rb
+++ spec/features/subscription_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
-RSpec.feature "Subscriptions", type: :feature do
-  context 'can be added' do
-    subject {  }
-    it 'by signed in user' do
+RSpec.feature "Subscription tests check that", type: :feature do
+  context 'signed in user' do
+    let(:user) { create(:user) }
+    let(:category) { create(:category) }
+    before(:each) { sign_in user }
+    it 'can subscribe on category' do
+      visit image_path(category)
+      click_link 'category-subscribe'
 
+      assert_text 'unsubscribe'
+    end
+    it 'can unsubscribe from category' do
+      visit image_path(category)
+      click_link 'category-unsubscribe'
+
+      assert_text 'subscribe'
     end
 
   end
