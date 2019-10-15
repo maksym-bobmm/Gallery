7503fab515bd5d9b66dea4b6ed964604f3299743
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-10-15 18:14:46 +0300

    Add few tests

--- spec/features/active_admin_spec.rb
+++ spec/features/active_admin_spec.rb
@@ -59,11 +59,50 @@ RSpec.feature "ActiveAdmins", type: :feature do
     before(:each) { sign_in admin }
     it 'filter can filter by user' do
       visit admin_categories_path
-      select text = category.user.email, from: 'q_owner_id'
+      select category.user.email, from: 'q_owner_id'
       click_link 'Filter'
 
       within '#main_content' do
         assert_text category.user.email
+      end
+    end
+  end
+  context 'test checks that admin users' do
+    let(:admin) { create(:admin) }
+    before(:each) { sign_in admin }
+    it 'can be created' do
+      visit admin_admin_users_path
+      click_link 'New Admin User'
+      fill_in 'admin_user_email', with: Faker::Internet.email
+      fill_in 'admin_user_password', with: 'password'
+      fill_in 'admin_user_password_confirmation', with: 'password'
+      click_link_or_button 'Create Admin user'
+
+      assert_text 'Admin user was successfully created.'
+    end
+    it 'can be filtered by email' do
+      admin2 = create(:admin)
+      visit admin_admin_users_path
+      within '.panel_contents' do
+        find('option', text: 'Equals').click
+        # select 'Equals', from: 'Contains'
+        fill_in 'q_email', with: admin2.email
+        click_link_or_button 'Filter'
+      end
+      within '#main_content' do
+        assert_text admin2.email
+      end
+    end
+    it 'can be deleted from list' do
+      email = create(:admin).email
+      visit admin_admin_users_path
+      within(:table_row, [email]) do
+        click_link class: 'delete_link'
+        page.driver.browser.switch_to.alert.accept
+      end
+
+      within '#main_content' do
+        assert_no_text email
       end
     end
   end
