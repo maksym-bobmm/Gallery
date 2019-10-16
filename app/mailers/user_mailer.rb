c77fcbf3ee69ebe925ee1487cea612e4bafd5ef9
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-10-16 14:34:16 +0300

    Move subscriptions email feature to resque

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,26 +4,19 @@
 class UserMailer < ApplicationMailer
   default from: 'notification@example.com'
 
-  def welcome_email
-    @user = params[:user]
-    @ulr = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-  end
-
   def new_image_in_category
-    # raise QWE
     users = User.joins(:subscriptions).where(subscriptions: { category_id: params[:category_id] }).pluck(:email)
     mail(to: users, subject: 'New Image on you subscriptions')
   end
 
   def subscriptions
-    @name = params[:category].name
+    @name = params[:category].fetch('name')
     user = params[:user]
     @is_subscribe = params[:subscribe]
     if @is_subscribe
-      mail(to: user.email, subject: 'Successful subscription')
+      mail(to: user.fetch('email'), subject: 'Successful subscription')
     else
-      mail(to: user.email, subject: 'Successful unsubscription')
+      mail(to: user.fetch('email'), subject: 'Successful unsubscription')
     end
   end
 end
