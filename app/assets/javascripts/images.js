032b7787e4b6f2d66cec55054b72b40b7702f0db
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-10-25 15:39:27 +0300

    Fix tests on ajaxed likes with rails assets:precompile

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,14 +1,12 @@
 $(document).on('turbolinks:load', function() {
     $(function() {
         $('body').on('ajax:success', '#image-unlike_link', function(event) {
-            debugger;
             $('#image-unlike_link').replaceWith(event.detail[0].link_with_image_tags);
             $('#likesCount').text(event.detail[0].likes_count + ' ' + event.detail[0].likes_word);
         });
     });
     $(function() {
         $('body').on('ajax:success', '#image-like_link', function(event) {
-            debugger;
             $('#image-like_link').replaceWith(event.detail[0].link_with_image_tags);
             $('#likesCount').text(event.detail[0].likes_count + ' ' + event.detail[0].likes_word);
         });
