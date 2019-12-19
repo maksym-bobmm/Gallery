1fd0fe8b63d82e230770006eaea901739df52208
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-12-19 12:46:03 +0200

    Change resque config
    Add Procfile

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-uri = URI.parse('redis://localhost:6379/')
-Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+if ENV["REDIS_URL"] && Rails.env.production?
+  Resque.redis = Redis.new(uri: ENV["REDIS_URL"])
+else
+  uri = URI.parse('redis://localhost:6379/')
+  Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+end
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
 Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
 Resque.logger.level = Logger::DEBUG
